6f175bb820bf00130c316a83bcdad0ddd0a45789
neel-am28 <neelamchaubey28@gmail.com>
2024-12-28 12:57:29 +0530

    added test case for single digit input

--- src/components/StringCalculator/StringCalculatos.test.js
+++ src/components/StringCalculator/StringCalculatos.test.js
@@ -6,4 +6,8 @@ describe("add function", () => {
   test("returns 0 for an empty string", () => {
     expect(add("")).toBe(0)
   })
+
+  test("returns first number if string length is 1", () => {
+    expect(add("1")).toBe(1)
+  })
 })
--- src/utils/add.js
+++ src/utils/add.js
@@ -2,4 +2,9 @@ export const add = (numString) => {
   if (numString.trim() == "") {
     return 0
   }
+  let numbersArray = []
+  numbersArray = numString.split("").map((nums) => Number(nums))
+  if (numbersArray.length == 1) {
+    return numbersArray[0]
+  }
 }
